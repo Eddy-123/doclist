8f7cc6043f70876be361c5bb6898914e0e1aa41f
Eddy-123 <adegnandjoueddy12@gmail.com>
2023-01-07 17:19:38 +0100

    creating a default collection

--- tasks/models.py
+++ tasks/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class Collection(models.Model):
     name = models.CharField(max_length=60)
     slug = models.SlugField()
+    @classmethod
+    def get_default_collection(cls) -> "Collection":
+        collection, _ = cls.objects.get_or_create(name="Defaut", slug="_defaut")
+        return collection
+    def __str__(self):
+        return self.name
 
 class Task(models.Model):
     description = models.CharField(max_length=300)
--- tasks/views.py
+++ tasks/views.py
@@ -6,12 +6,13 @@ from django.utils.html import escape
 # Create your views here.
 def index(request):
     context = {}
-    context['collections'] = Collection.objects.order_by("name")
+    collection = Collection.get_default_collection()
+    context['collections'] = Collection.objects.order_by("slug")
     return render(request, 'tasks/index.html', context=context)
 
 def add_collection(request):
     collection_name = escape(request.POST.get("collection-name"))
     collection, created = Collection.objects.get_or_create(name=collection_name)
     if not created:
-        return HttpResponse("La collection existe deja")
-    return redirect('home')
+        return HttpResponse("La collection existe deja", status=409)
+    return HttpResponse(f'<h2>{collection_name}</h2>')
